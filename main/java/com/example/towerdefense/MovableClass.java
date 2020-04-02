9ae44055184100ca6cd42ac1af15ec74592cf12b
Quinn Roemer <40554666+Warthog710@users.noreply.github.com>
2020-04-02 12:36:50 -0700

    Renamed to Movable

--- main/java/com/example/towerdefense/MovableClass.java
+++
@@ -1,8 +0,0 @@
-package com.example.towerdefense;
-
-public abstract class MovableClass extends GameObject
-{
-    Angle mHeading; //angle that the object is heading
-    int mSpeed; //speed
-    abstract void move(); //how it moves
-}
